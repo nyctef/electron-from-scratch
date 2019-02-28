2702adc75f78058992b7c9f5ed585629007aff73
Nyctef <nyctef@nyctef.com>
2019-02-28 15:59:17 +0000

    use React.FunctionComponent type so props type is inferred correctly

--- src/website/index.tsx
+++ src/website/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 
 const app = document.getElementById('app');
 
-const Emphasis = (props: any) => <em>{props.children}</em>;
+const Emphasis: React.FunctionComponent = props => <em>{props.children}</em>;
 
 const App = () => (
   <div>
